a6ed80d2efb99b214eed3e779699fffc1d7f7972
meet chauhan <meetchauhan9915@gmail.com>
2025-08-11 14:08:00 +0530

    Initial commit

--- userCollection/src/main/java/com/userProjects/userCollection/UserCollectionApplication.java
+++ userCollection/src/main/java/com/userProjects/userCollection/UserCollectionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserCollectionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserCollectionApplication.class, args);
 	}
